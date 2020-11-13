37d4e1c11715e7ea20916f26fdd0092287637944
mdjmc1 <mdjmc1@163.com>
2020-11-13 20:59:48 +0800

    灰度值要从0开始，bug修复，暂时写死

--- rgb2gray.py
+++ rgb2gray.py
@@ -1,6 +1,7 @@
 import os
 import PIL.Image as Image
-
+import numpy as np
+import time
 
 def rgb2gray(rgbfile, pngfile):
     I = Image.open(rgbfile)
@@ -8,6 +9,33 @@ def rgb2gray(rgbfile, pngfile):
     L = I.convert('L')
     # L.show()
     L.save(pngfile)
+
+
+def replace_color(img, src_clr, dst_clr):
+    ''' 通过矩阵操作颜色替换程序
+    @param	img:	图像矩阵
+    @param	src_clr:	需要替换的颜色(r,g,b)
+    @param	dst_clr:	目标颜色		(r,g,b)
+    @return				替换后的图像矩阵
+    '''
+
+    img_arr = np.asarray(img, dtype=np.double)
+
+    r_img = img_arr[:, :, 0].copy()
+    g_img = img_arr[:, :, 1].copy()
+    b_img = img_arr[:, :, 2].copy()
+
+    img = r_img * 256 * 256 + g_img * 256 + b_img
+    src_color = src_clr[0] * 256 * 256 + src_clr[1] * 256 + src_clr[2]  # 编码
+
+    r_img[img == src_color] = dst_clr[0]
+    g_img[img == src_color] = dst_clr[1]
+    b_img[img == src_color] = dst_clr[2]
+
+    dst_img = np.array([r_img, g_img, b_img], dtype=np.uint8)
+    dst_img = dst_img.transpose(1, 2, 0)
+
+    return dst_img
 
 
 # 遍历文件夹
@@ -21,7 +49,17 @@ def walkFile(rgbpath, graypath):
         # 遍历文件
         for f in files:
             if f.endswith(".png") or f.endswith(".tif"):
-                rgb2gray(os.path.join(root, f), os.path.join(graypath, f))
+
+                #to do 这里默认选择白色替换成(1，1，1)仅替换一次, 以后应该遍历所有颜色，从(0,0,0)开始重新赋值(1,1,1)、(2,2,2)递增
+                img = Image.open(os.path.join(root, f)).convert('RGB')
+                res_img = img.copy()
+                dst_img = replace_color(img, (255, 255, 255), (1, 1, 1))
+                res_img = dst_img
+                res_img = Image.fromarray(res_img)
+                res_img.save(os.path.join(graypath, f))
+
+
+                rgb2gray(os.path.join(graypath, f), os.path.join(graypath, f))
 
 
 if __name__ == "__main__":
